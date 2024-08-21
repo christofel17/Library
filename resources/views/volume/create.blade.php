0868657f92eafad9e002a96bb3812a91355d2c19
christofel <christofel.pardede@gmail.com>
2024-08-21 15:12:27 +0700

    Dropdown

--- resources/views/volume/create.blade.php
+++ resources/views/volume/create.blade.php
@@ -14,16 +14,18 @@
             </div>
             <!-- /.card-header -->
             <!-- form start -->
-            <form action="/volume/create/{{ $book->slug }}" method="post">
+            <form action="/volume/create/{{ $book->slug }}" method="post" id="volumeform">
                 @csrf
               <div class="card-body">
                 <div class="form-group">
-                  <input type="text" name="format" class="form-control @error('format') is-invalid @enderror" id="format" placeholder="Format" value="{{ old('format') }}">
-                  @error('format')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                    @enderror
+                  <select name="format" class="form-select" id="format">
+                    <option value="">Format</option>
+                    <option value="Hardcover">Hardcover</option>
+                    <option value="Paperback">Paperback</option>
+                    <option value="Softcover">Softcover</option>
+                    <option value="E-book">E-book</option>
+                    <option value="Audiobook">Audiobook</option>
+                  </select>
                 </div>
                 <div class="form-group">
                     <input type="text" name="edition" class="form-control @error('edition') is-invalid @enderror" id="edition" placeholder="Edition" value="{{ old('edition') }}">
@@ -34,13 +36,22 @@
                     @enderror
                   </div>
                   <div class="form-group">
-                    <input type="text" name="state" class="form-control @error('state') is-invalid @enderror" id="state" placeholder="Condition" value="{{ old('state') }}">
-                    @error('state')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                    @enderror
+                    <select name="state" class="form-select" id="state">
+                      <option value="">Condition</option>
+                      <option value="New">New</option>
+                      <option value="Very Good">Very Good</option>
+                      <option value="Good">Good</option>
+                      <option value="Fair">Fair</option>
+                      <option value="Poor">Poor</option>
+                      <option value="Digital">Digital</option>
+                    </select>
                   </div>
+                
+                    <!--
+                    <input type="text" name="state" class="form-control @error('state') is-invalid @enderror" id="state" placeholder="Condition" value="{{ old('state') }}">
+                    <div class="invalid-feedback">
+                    </div>
+                    -->
                 <div class="form-check">
                   <input type="checkbox" class="form-check-input" id="exampleCheck1">
                   <label class="form-check-label" for="exampleCheck1">Check me out</label>
